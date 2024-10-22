c9f47da75911fa1ac19d066b0324fac81a7649bb
evgeny <evgeny@158.160.8.209>
2024-10-22 10:07:42 +0700

    деплой проекта

--- dist/6ac1a212df40e9805cc5.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router/router.js';
-import '../src/assets/global.scss';
-new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app');
