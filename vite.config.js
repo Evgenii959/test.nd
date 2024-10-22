02f805380ec54c374833a7c028058034a3b99c24
evgeny <evgeny@158.160.8.209>
2024-10-23 01:29:16 +0700

    сборка проекта

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import path from 'path';
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/test.nd/' : '/',
   plugins: [createVuePlugin()],
   server: {
     port: 8080,
